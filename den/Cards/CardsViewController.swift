ba0729e74cd3124280910551d9b4b7ad7f1d454c
thatartplace <49554510+thatartplace@users.noreply.github.com>
2019-06-01 23:44:48 -0400

    collection view setup

---
+++ den/Cards/CardsViewController.swift
@@ -0,0 +1,11 @@
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
