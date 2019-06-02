a94ad716211eef86c6d0b8ce914d2ac8959fea7f
thatartplace <49554510+thatartplace@users.noreply.github.com>
2019-06-02 00:08:54 -0400

    @discardable ++--

--- den/operators/++--.swift
+++ den/operators/++--.swift
@@ -8,22 +8,26 @@ prefix operator --
 postfix operator --
 
 extension Int {
+    @discardableResult
     static prefix func ++ (operand: inout Int) -> Int {
         operand += 1
         return operand
     }
     
+    @discardableResult
     static postfix func ++ (operand: inout Int) -> Int {
         let old = operand
         operand += 1
         return old
     }
     
+    @discardableResult
     static prefix func -- (operand: inout Int) -> Int {
         operand -= 1
         return operand
     }
     
+    @discardableResult
     static postfix func -- (operand: inout Int) -> Int {
         let old = operand
         operand -= 1
