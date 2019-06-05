339a7dde1a8c8aa3cd0dfb5bb8c1b5b5931ef36a
thatartplace <49554510+thatartplace@users.noreply.github.com>
2019-06-05 12:22:35 -0400

    fix wrong index used to create cell attributes

--- den/Cards/CardsLayout.swift
+++ den/Cards/CardsLayout.swift
@@ -11,7 +11,7 @@ struct CellAttributesInvariants {
     var size: CGSize
     var exposedHeight: CGFloat
     var stackedHeight: CGFloat
-    var zIndex: Int
+    var index: Int
 }
 
 class CardsLayout: UICollectionViewLayout {
@@ -41,7 +41,7 @@ class CardsLayout: UICollectionViewLayout {
             return CellAttributesInvariants(size: CGSize(width: cView.bounds.width, height: height),
                                             exposedHeight: exposed,
                                             stackedHeight: stackedHeight,
-                                            zIndex: indexPath.row)
+                                            index: indexPath.item)
         }
     }
     
@@ -56,19 +56,18 @@ class CardsLayout: UICollectionViewLayout {
                 return false
             }
             else {
-                springDistance ?= invariant.stackedHeight - rect.origin.y
+                springDistance ?= invariant.stackedHeight - rect.minY
             }
             if invariant.stackedHeight > rect.maxY + invariant.exposedHeight {
                 return false
             }
             return true
         }
-        print("in rect: \(inRect.enumerated().map { $0.1.zIndex })")
-        return inRect.enumerated().map { i, invariant in
-            let attributes = UICollectionViewLayoutAttributes(forCellWith: IndexPath(item: i, section: 0))
+        return inRect.map { invariant in
+            let attributes = UICollectionViewLayoutAttributes(forCellWith: IndexPath(item: invariant.index, section: 0))
             let origin = CGPoint(x: 0, y: invariant.stackedHeight - invariant.exposedHeight)
             attributes.frame = CGRect(origin: origin, size: invariant.size)
-            attributes.zIndex = invariant.zIndex
+            attributes.zIndex = invariant.index
             return attributes
         }
     }
