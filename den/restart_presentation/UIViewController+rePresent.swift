ad06570c457595e973a44c03aa9509921da92ed9
thatartplace <49554510+thatartplace@users.noreply.github.com>
2019-07-08 11:34:44 -0400

    improved represent

--- den/restart_presentation/UIViewController+rePresent.swift
+++ den/restart_presentation/UIViewController+rePresent.swift
@@ -3,25 +3,40 @@
 import UIKit
 
 extension UIViewController {
-    func rePresent(animation: RePresentTransitionAnimation, completion: (() -> Void)? = nil) {
+    /// first dismiss, then present the presented view controller
+    /// if the callee isn't presenting any view controller, the call
+    /// is recursively passed along to the callee's presenting view
+    /// controller, if one exists.
+    /// - parameters:
+    ///     - pre: the presented vc is passed as argument
+    ///     - post: called after all animations finish. presented vc is passed as argument
+    /// - returns:
+    ///     the presented vc, nil if callee is not part of any presentation
+    @discardableResult
+    func rePresent(animation: RePresentTransitionAnimation,
+                   pre: ((UIViewController) -> Void)? = nil,
+                   post: ((UIViewController) -> Void)? = nil) -> UIViewController? {
         guard let presented = presentedViewController else {
-            presentingViewController?.rePresent(animation: animation, completion: completion)
-            return
+            return presentingViewController?.rePresent(animation: animation, pre: pre, post: post)
         }
         guard let controller = presented.presentationController, controller.presentingViewController == self else {
-            return
+            return nil
         }
+        
+        pre?(presented)
         
         let currentFrame = controller.frameOfPresentedViewInContainerView
         let delegate = Unmanaged.passRetained(RePresentTransitionDelegate(animation: animation, currentFrame: currentFrame))
         let savedDelegate = presented.transitioningDelegate
         
         presented.transitioningDelegate = delegate.takeUnretainedValue()
-        dismiss(animated: animation.animateDismiss)
-        present(presented, animated: animation.animatePresent) {
-            delegate.release()
-            presented.transitioningDelegate = savedDelegate
-            completion?()
+        dismiss(animated: animation.animateDismiss) {
+            self.present(presented, animated: animation.animatePresent) {
+                delegate.release()
+                presented.transitioningDelegate = savedDelegate
+                post?(presented)
+            }
         }
+        return self
     }
 }
