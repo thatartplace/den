520dbd28cb10e5104bb10308102bcc1c1ed860d3
thatartplace <49554510+thatartplace@users.noreply.github.com>
2019-07-16 19:04:01 -0400

    no more fsm for now

--- den/fsm/FSMState.swift
+++ den/fsm/FSMState.swift
@@ -9,3 +9,9 @@ protocol FSMState: Hashable {
     static var acceptingStates: Set<Self> { get }
     func transition(using: Input) -> Self
 }
+
+extension FSMState {
+    var accepted: Bool {
+        return Self.acceptingStates.contains(self)
+    }
+}
